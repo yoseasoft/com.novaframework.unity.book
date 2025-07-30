c083ad164bd39b49852aedea5ba7fe63da907a9b
hurley <yoseasoft@163.com>
2025-07-30 20:24:02 +0800

    部署 book 模块；

---
+++ BookObject.cs
@@ -0,0 +1,14 @@
+
+namespace Game.Pack
+{
+    /// <summary>
+    /// 演示案例总控
+    /// </summary>
+    public static class BookObject
+    {
+        public static void ReadBook(string str)
+        {
+            UnityEngine.Debug.Log(str);
+        }
+    }
+}
